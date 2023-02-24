327683c31a6c68e452c57f9ff756fd87a81047b9
bowenliee <98018004+bowenliee@users.noreply.github.com>
2023-02-24 13:53:47 +0800

    Delete MARK55_BN.py

--- MARK55_BN.py
+++
@@ -1,6 +0,0 @@
-#encoding:utf-8
-import numpy as np
-def MARK55_BN(X_local,gamma,beta,m,sigma):
-	Ns = np.shape(X_local)[0]
-	X_local_BN = gamma * (X_local - np.ones((Ns, 1)) * m) / np.sqrt(np.ones((Ns, 1)) * sigma ) + beta
-	return X_local_BN
